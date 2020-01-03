e72cbc8237faadb307fd8651b44c89ab0b95b4c5
Luis Oyanguren <leog.1992@gmail.com>
2020-01-03 16:52:16 -0500

    ver mis bancos

--- models/Banco.php
+++ models/Banco.php
@@ -6,9 +6,8 @@ class Banco
 {
     private $id_banco;
     private $nombre;
-    private $efectivo;
-    private $virtual;
-    private $id_empresa;
+    private $nrocuenta;
+    private $monto;
     private $c_conectar;
 
     /**
@@ -54,79 +53,62 @@ class Banco
     /**
      * @return mixed
      */
-    public function getEfectivo()
+    public function getNrocuenta()
     {
-        return $this->efectivo;
+        return $this->nrocuenta;
     }
 
     /**
-     * @param mixed $efectivo
+     * @param mixed $nrocuenta
      */
-    public function setEfectivo($efectivo)
+    public function setNrocuenta($nrocuenta)
     {
-        $this->efectivo = $efectivo;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getVirtual()
-    {
-        return $this->virtual;
-    }
-
-    /**
-     * @param mixed $virtual
-     */
-    public function setVirtual($virtual)
-    {
-        $this->virtual = $virtual;
+        $this->nrocuenta = $nrocuenta;
     }
 
     /**
      * @return mixed
      */
-    public function getIdEmpresa()
+    public function getMonto()
     {
-        return $this->id_empresa;
+        return $this->monto;
     }
 
     /**
-     * @param mixed $id_empresa
+     * @param mixed $monto
      */
-    public function setIdEmpresa($id_empresa)
+    public function setMonto($monto)
     {
-        $this->id_empresa = $id_empresa;
+        $this->monto = $monto;
     }
 
     public function generarCodigo()
     {
-        $sql = "select ifnull(max(id_banco) +1, 1) as codigo from banco";
+        $sql = "select ifnull(max(id_banco) +1, 1) as codigo from caja_bancos";
         $this->id_banco = $this->c_conectar->get_valor_query($sql, "codigo");
     }
 
     public function obtenerDatos()
     {
-        $sql = "select * from banco 
+        $sql = "select * from caja_bancos 
         where id_banco = '$this->id_banco'";
         $resultado = $this->c_conectar->get_Row($sql);
         $this->nombre = $resultado['nombre'];
-        $this->efectivo = $resultado['efectivo'];
-        $this->virtual = $resultado['virtual'];
+        $this->nrocuenta = $resultado['nro_cuenta'];
+        $this->monto = $resultado['monto'];
     }
 
 
     public function insertar()
     {
-        $sql = "insert into banco values ('$this->id_banco', '$this->nombre', '$this->efectivo', '$this->virtual', '$this->id_empresa')";
+        $sql = "insert into caja_bancos values ('$this->id_banco', '$this->nombre', '$this->efectivo', '$this->virtual', '$this->id_empresa')";
         return $this->c_conectar->ejecutar_idu($sql);
     }
 
     public function verFilas()
     {
         $sql = "select * 
-        from banco 
-        where id_empresa = '$this->id_empresa'";
+        from caja_bancos ";
         return $this->c_conectar->get_Cursor($sql);
     }
 
